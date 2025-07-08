847aee28a427e38159cd41fcbacbf8575f34705f
Sagaradyan <sagarkadyan823@gmail.com>
2025-07-08 18:38:51 +0530

    new key

--- aymclient.py
+++ aymclient.py
@@ -1,17 +1,19 @@
+
 import rsa
 
-public_key, private_key = rsa.newkeys(1024)
+# Only run this part once to generate new key files
+'''public_key, private_key = rsa.newkeys(1024)
+with open("public.pem", "wb") as f:
+    f.write(public_key.save_pkcs1("PEM"))
+with open("private.pem", "wb") as f:
+    f.write(private_key.save_pkcs1("PEM"))
+'''
+# Now read the keys back (make sure files exist and are correct)
+with open("public.pem", "rb") as f:
+    public_key = rsa.PublicKey.load_pkcs1(f.read())
 
-print(private_key)
+with open("private.pem", "rb") as f:
+    private_key = rsa.PrivateKey.load_pkcs1(f.read())
 
-with open("public.pem","rb") as f:
-    public_key =rsa.PublicKey.load_pkcs1(f.read())
-
-
-with open("private.pem","rb") as f:
-    public_key=rsa.Private_key.load_pkcs1(f.read())
 
 message = "hello"
-
-encrypted_message= rsa.encrypt(message.encode(),public_key)
-print(encrypted_message)
